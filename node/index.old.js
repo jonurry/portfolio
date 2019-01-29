47f1ea1061ac2b021225e30cf8cb5d86dfe912f9
Jon Urry <jon@urry.me>
2019-01-29 16:29:32 +0000

    Delete node server files for sending emails
    These have been replaced by the AWS Lambda function

--- node/index.old.js
+++
@@ -1,9 +0,0 @@
-const server = require('./server');
-const router = require('./router');
-const requestHandlers = require('./request-handlers');
-
-let handle = {};
-handle['/'] = requestHandlers.start;
-handle['/upload'] = requestHandlers.upload;
-handle['/sendmail'] = requestHandlers.sendEmail;
-server.start(router.route, handle);
